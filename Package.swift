f51d004241a008d4ca08e7a6c7eac076c66e4dd4
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-07 20:54:33 +0200

    init

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IncomeAllocator",
+    platforms: [
+        .macOS(.v15)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/leviouwendijk/plate.git", branch: "master"),
+        .package(url: "https://github.com/leviouwendijk/Economics.git", branch: "master"),
+        // .package(url: "https://github.com/leviouwendijk/Interfaces.git", branch: "master"),
+        .package(url: "https://github.com/leviouwendijk/ViewComponents.git", branch: "master"),
+        .package(url: "https://github.com/leviouwendijk/Compositions.git", branch: "master"),
+        .package(url: "https://github.com/leviouwendijk/Implementations.git", branch: "master"),
+        // .package(url: "https://github.com/leviouwendijk/Structures.git", branch: "master"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "IncomeAllocator",
+            dependencies: [
+                .product(name: "plate", package: "plate"),
+                .product(name: "Economics", package: "Economics"),
+                // .product(name: "Interfaces", package: "Interfaces"),
+                .product(name: "ViewComponents", package: "ViewComponents"),
+                .product(name: "Compositions", package: "Compositions"),
+                .product(name: "Implementations", package: "Implementations"),
+                // .product(name: "Structures", package: "Structures"),
+            ],
+            // resources: [
+            //     .process("Resources"),
+            // ]
+        ),
+    ]
+)
---
+++ Sources/app.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+import plate
+import Interfaces
+import ViewComponents
+import Compositions
+import Implementations
+// import Interfaces
+
+@main
+struct ResponderApp: App {
+    @StateObject private var viewmodel = ResponderViewModel()
+
+    init() {
+        NSWindow.allowsAutomaticWindowTabbing = false
+    }
+
+    @State private var selectedTab: Int = 0
+
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                TabView(selection: $selectedTab) {
+                    IncomeAllocatorView()
+                      // .environmentObject(viewmodel)
+                      .tabItem {
+                          Label("Allocator", systemImage: "paperplane.fill")
+                      }
+                      .tag(0)
+
+                    // QuotaView(viewmodel: viewmodel)
+                    //   .tabItem {
+                    //       Label("Quota", systemImage: "list.bullet")
+                    //   }
+                    //   .tag(1)
+
+                    // MailerStandardOutput()
+                    //   .environmentObject(viewmodel)
+                    //   .tabItem {
+                    //       Label("stdout", systemImage: "terminal.fill")
+                    //   }
+                    //   .tag(2)
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+
+                BuildInformationSwitch(
+                    alignment: .center,
+                    display: [
+                        [.version],
+                        [.latestVersion],
+                        [.name],
+                        [.author]
+                    ],
+                    prefixStyle: .long
+                )
+            }
+        }
+    }
+}
+
